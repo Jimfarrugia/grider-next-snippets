47928334c30764b94dd4395829aef60fab2a8f66
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-08 16:31:41 +1000

    add error page and error handler to create snippet method

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -21,25 +21,37 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  // validate the user's input
-  const title = formData.get("title");
-  const code = formData.get("code");
+  try {
+    // validate the user's input
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-  if (typeof title !== "string" || title.length < 3) {
-    return { message: "Title must be longer." };
+    if (typeof title !== "string" || title.length < 3) {
+      return { message: "Title must be longer." };
+    }
+
+    if (typeof code !== "string" || code.length < 10) {
+      return { message: "Code must be longer." };
+    }
+
+    // create a new record in the db
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
   }
-
-  if (typeof code !== "string" || code.length < 10) {
-    return { message: "Code must be longer." };
-  }
-
-  // create a new record in the db
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
 
   // redirect the user to the homepage
   redirect("/");
