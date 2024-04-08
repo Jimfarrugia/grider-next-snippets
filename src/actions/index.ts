aafd359a1b83942c4089a93b22b016cbabcda33f
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-08 16:20:59 +1000

    extract create snippet method to server action and validate input

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -16,3 +16,31 @@ export async function deleteSnippet(id: number) {
   await db.snippet.delete({ where: { id } });
   redirect("/");
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  // validate the user's input
+  const title = formData.get("title");
+  const code = formData.get("code");
+
+  if (typeof title !== "string" || title.length < 3) {
+    return { message: "Title must be longer." };
+  }
+
+  if (typeof code !== "string" || code.length < 10) {
+    return { message: "Code must be longer." };
+  }
+
+  // create a new record in the db
+  const snippet = await db.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+
+  // redirect the user to the homepage
+  redirect("/");
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,29 +1,15 @@
-import { redirect } from "next/navigation";
-import { db } from "@/db";
+"use client";
+
+import { useFormState } from "react-dom";
+import * as actions from "@/actions";
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    // this needs to be a server action
-    "use server";
-
-    // validate the user's input
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    // create a new record in the db
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    // redirect the user to the homepage
-    redirect("/");
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: "",
+  });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -47,6 +33,11 @@ export default function SnippetCreatePage() {
             className="border rounded p-2 w-full"
           />
         </div>
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+            {formState.message}
+          </div>
+        ) : null}
         <button type="submit" className="rounded p-2 bg-blue-200">
           Submit
         </button>
