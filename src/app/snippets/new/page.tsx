5c204baea1825c9ddc4bbe17691286956700a0df
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-08 10:51:10 +1000

    add server action to create a snippet

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,30 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // this needs to be a server action
+    "use server";
+
+    // validate the user's input
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // create a new record in the db
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+
+    // redirect the user to the homepage
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
